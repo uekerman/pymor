8360ee85a07591188dd8639e5541ef09ff195c8d
Stephan Rave <stephanrave@uni-muenster.de>
2021-02-23 14:44:27 +0100

    [appimage] proof-of-concept build script

---
+++ appimage/AppRun.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+this_dir=$(dirname "$0")
+export PATH="$this_dir"/usr/bin:"$PATH"
+export PKG_CONFIG_PATH="$this_dir"/usr/conda/lib/pkgconfig
+
+# strace "$this_dir"/usr/bin/python "$this_dir"/script.py "$@"
+# "$this_dir"/usr/bin/python -m pdb "$this_dir"/usr/bin/jupyter-qtconsole "$@"
+# python "$this_dir"/usr/bin/jupyter-qtconsole "$@"
+python "$this_dir"/usr/bin/jupyter-notebook "$@"
---
+++ appimage/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+wget -c "https://raw.githubusercontent.com/TheAssassin/linuxdeploy-plugin-conda/master/linuxdeploy-plugin-conda.sh"
+wget -c "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
+chmod u+x linuxdeploy-x86_64.AppImage linuxdeploy-plugin-conda.sh
+
+export CONDA_CHANNELS="conda-forge"
+export CONDA_DOWNLOAD_DIR=/home/stephan/Repos/pymor/appimage/conda
+export CONDA_PACKAGES="pymor;fenics"
+
+./linuxdeploy-x86_64.AppImage --appdir AppDir -i pymor.png -d pymor.desktop --plugin conda --output appimage --custom-apprun AppRun.sh
+
+cp qt.conf AppDir/usr/bin/
+cp kernel.json AppDir/usr/conda/share/jupyter/kernels/python3/
+
+./linuxdeploy-x86_64.AppImage --appdir AppDir  --output appimage
